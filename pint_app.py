c328ac83bd552d8c13dbc0942c9100ce10f8eca7
Mr-Scarf <dscal07@outlook.com>
2026-04-06 15:32:17 +0100

    Updated pint.app & static pages (index) + tested crud operations

--- pint_app.py
+++ pint_app.py
@@ -2,36 +2,51 @@
 # 22nd March - starting skeleton for project from lab 5
 # Will do pints prices of pubs in maynooth
 
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, url_for, request, redirect, abort , jsonify
+from pint_dao import pintDAO
+from flask import send_from_directory
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
 
 @app.route('/')
 def index():
-    return "Hello, Pints!"
+    return send_from_directory('staticpages', 'index.html')
 
 @app.route('/pints', methods=['GET'])
 def getall():
-    return "get all pints"
+    results = pintDAO.getAll()
+    return jsonify(results)
+    #return "get all pints"
 
 @app.route('/pints/<int:id>', methods=['GET'])
 def findbyid(id):
-    return f"find by id {id}"
+    result = pintDAO.findByID(id)
+    return jsonify(result)
+    #return f"find by id {id}"
 
 
 @app.route('/pints', methods=['POST'])
 def create():
-    jsonstring = request.json
-    return f"create {jsonstring}"
+    pint = request.json
+    created = pintDAO.create(pint)
+    return jsonify(created)
+    
+    #jsonstring = request.json
+    #return f"create {jsonstring}"
 
 @app.route('/pints/<int:id>', methods=['PUT'])
 def update(id):
-    jsonstring = request.json
-    return f"update {id} {jsonstring}"
+    pint = request.json
+    updated = pintDAO.update(id, pint)
+    return jsonify(updated)
+    #jsonstring = request.json
+    #return f"update {id} {jsonstring}"
 
 @app.route('/pints/<int:id>', methods=['DELETE'])
 def delete(id):
-    return f"delete {id}"
+    pintDAO.delete(id)
+    return jsonify({"done": True})
+    #return f"delete {id}"
 
 if __name__ == '__main__':
     app.run(debug=True) 
